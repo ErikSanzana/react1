3e9ce6831c4360afb905545fff65fe440911f045
ErikSanzana <ersanzana@gmail.com>
2023-08-17 08:23:29 -0400

    ahora las cartas estan al lado de otra

--- src/App.jsx
+++ src/App.jsx
@@ -10,38 +10,58 @@ import tres from "./assets/imgs/tres.jpg";
 import cuatro from "./assets/imgs/cuatro.jpg";
 
 function App() {
+  const dogData = [
+    {
+      image: uno,
+      name: "perros de carrera",
+      description: "perros altamente capacitados para largas distancias",
+      tagText: "Carrera",
+      tagColor: "dark",
+    },
+    {
+      image: dos,
+      name: "perros de caza",
+      description: "perros altamente capacitados para atacar motochorros",
+      tagText: "Caza",
+      tagColor: "danger",
+    },
+    {
+      image: tres,
+      name: "perros cieguitos",
+      description: "perros adiestrados en el arte de guiar personas con discapacidad",
+      tagText: "Ciego",
+      tagColor: "info",
+    },
+    {
+      image: cuatro,
+      name: "perros de guerra",
+      description: "perros entrenados militarmente para conducir tanques sovieticos",
+      tagText: "Guerra",
+      tagColor: "warning",
+    },
+  ];
+
   return (
     <>
       <Header title="Adopta un perrito" />
 
-      <DogCard
-        image={uno}
-        name="perros de carrera"
-        description="perros altamente capacitados para largas distancias"
-        tagText="Carrera"
-        tagColor="dark"
-      />
-      <DogCard
-        image={dos}
-        name="perros de caza"
-        description="perros altamente capacitados para atacar motochorros"
-        tagText="Caza"
-        tagColor="danger"
-      />
-      <DogCard
-        image={tres}
-        name="perros cieguitos"
-        description="perros adiestrados en el arte de guiar personas con discapacidad"
-        tagText="Ciego"
-        tagColor="info"
-      />
-      <DogCard
-        image={cuatro}
-        name="perros de guerra"
-        description="perros entrenados militarmente para conducir tanques sovieticos"
-        tagText="Guerra"
-        tagColor="warning"
-      />
+      <div className="container">
+        <div className="row">
+          {dogData.map((dog, index) => (
+            <div className="col-md-3" key={index}>
+              {/* vuelve el orden */}
+              <DogCard
+                image={dog.image}
+                name={dog.name}
+                description={dog.description}
+                tagText={dog.tagText}
+                tagColor={dog.tagColor}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
       <Footer text="ASOCIACION DE PERROS S.A" />
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,19 @@ import BootstrapCard from "react-bootstrap/Card";
 import Tag from "./Tags"; 
 
 function DogCard({ name, image, description, tagText, tagColor }) {
+  /* ver video para ver como se tabula mas rapido esto */
   return (
     <BootstrapCard style={{ width: "18rem" }}>
+      {/* tamaño */}
       <BootstrapCard.Img variant="top" src={image} />
+      {/* lugar y que */}
       <BootstrapCard.Body>
         <BootstrapCard.Title>{name}</BootstrapCard.Title>
+        {/* que va en el titulo */}
         <BootstrapCard.Text>{description}</BootstrapCard.Text>
+        {/* que va en el campo texto */}
         <Tag text={tagText} color={tagColor} />
+        {/* ocupando tag */}
       </BootstrapCard.Body>
     </BootstrapCard>
   );
